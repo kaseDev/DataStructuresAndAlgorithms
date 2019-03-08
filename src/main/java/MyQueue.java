7652645f57f82ceb9e3a1237ce128d04f977af31
kaseDev <rick.kase@gmail.com>
2019-03-08 03:12:59 -0500

    implemented a Queue.

---
+++ src/main/java/MyQueue.java
@@ -0,0 +1,38 @@
+public class MyQueue<T> {
+
+	private Node<T> head;
+	private Node<T> tail;
+
+	public void enqueue(T element) {
+		if (head == null) {
+			head = new Node<>(element);
+			tail = head;
+			return;
+		}
+		tail.next = new Node<>(element);
+		tail = tail.next;
+	}
+
+	public T dequeue() {
+		if (head == null)
+			return null;
+		T value = head.value;
+		head = head.next;
+		return value;
+	}
+
+	public T peek() {
+		if (head == null)
+			return null;
+		return head.value;
+	}
+
+	private class Node<K> {
+		private K value;
+		private Node<K> next;
+
+		private Node(K value) {
+			this.value = value;
+		}
+	}
+}
